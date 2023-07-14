c80c64f56c74f38bcc52a302a3d75aaea624e2e9
Art <121850372+arterxsk@users.noreply.github.com>
2023-07-14 03:13:43 -0700

    update modules/checker/sm.php

--- modules/checker/sm.php
+++ modules/checker/sm.php
@@ -17,115 +17,115 @@ include_once __DIR__."/../functions/functions.php";
 
 
 ////////////====[MUTE]====////////////
-if(strpos($message, "/si ") === 0 || strpos($message, "!si ") === 0){   
-    $antispam = antispamCheck($userId);
-    addUser($userId);
-    
-    if($antispam != False){
-      bot('sendmessage',[
-        'chat_id'=>$chat_id,
-        'text'=>"[<u>ANTI SPAM</u>] Try again after <b>$antispam</b>s.",
-        'parse_mode'=>'html',
-        'reply_to_message_id'=> $message_id
-      ]);
-      return;
+if (strpos($message, "/si ") === 0 || strpos($message, "!si ") === 0) {
+ $antispam = antispamCheck($userId);
+ addUser($userId);
 
-    }else{
-        $messageidtoedit1 = bot('sendmessage',[
-          'chat_id'=>$chat_id,
-          'text'=>"<b>Wait for Result...</b>",
-          'parse_mode'=>'html',
-          'reply_to_message_id'=> $message_id
+ if ($antispam != False) {
+  bot('sendmessage', [
+   'chat_id' => $chat_id,
+   'text' => "[<u>ANTI SPAM</u>] Try again after <b>$antispam</b>s.",
+   'parse_mode' => 'html',
+   'reply_to_message_id' => $message_id
+  ]);
+  return;
 
-        ]);
+ } else {
+  $messageidtoedit1 = bot('sendmessage', [
+   'chat_id' => $chat_id,
+   'text' => "<b>Wait for Result...</b>",
+   'parse_mode' => 'html',
+   'reply_to_message_id' => $message_id
 
-        $messageidtoedit = capture(json_encode($messageidtoedit1), '"message_id":', ',');
-        $lista = substr($message, 4);
-        $bin = substr($cc, 0, 6);
-        
-        if(preg_match_all("/(\d{16})[\/\s:|]*?(\d\d)[\/\s|]*?(\d{2,4})[\/\s|-]*?(\d{3})/", $lista, $matches)) {
-            $creditcard = $matches[0][0];
-            $cc = multiexplode(array(":", "|", "/", " "), $creditcard)[0];
-            $mon = multiexplode(array(":", "|", "/", " "), $creditcard)[1];
-            $year = multiexplode(array(":", "|", "/", " "), $creditcard)[2];
-            $cvv = multiexplode(array(":", "|", "/", " "), $creditcard)[3];
-            $sk = $config['sk_keys'];
-            shuffle($sk);
-            $sec = $sk[0];
+  ]);
 
-            file_put_contents('sk.txt',$sk);
-    
-            ###CHECKER PART###  
-            
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'https://lookup.binlist.net/'.$cc.'');
-            curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Host: lookup.binlist.net',
-            'Cookie: _ga=GA1.2.549903363.1545240628; _gid=GA1.2.82939664.1545240628',
-            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'));
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, '');
-            $fim = curl_exec($ch);
-            $bank = capture($fim, '"bank":{"name":"', '"');
-            $cname = capture($fim, '"name":"', '"');
-            $brand = capture($fim, '"brand":"', '"');
-            $country = capture($fim, '"country":{"name":"', '"');
-            $phone = capture($fim, '"phone":"', '"');
-            $scheme = capture($fim, '"scheme":"', '"');
-            $type = capture($fim, '"type":"', '"');
-            $emoji = capture($fim, '"emoji":"', '"');
-            $currency = capture($fim, '"currency":"', '"');
-            $binlenth = strlen($bin);
-            $schemename = ucfirst("$scheme");
-            $typename = ucfirst("$type");
-            
-            
-            /////////////////////==========[Unavailable if empty]==========////////////////
-            
-            
-            if (empty($schemename)) {
-            	$schemename = "Unavailable";
-            }
-            if (empty($typename)) {
-            	$typename = "Unavailable";
-            }
-            if (empty($brand)) {
-            	$brand = "Unavailable";
-            }
-            if (empty($bank)) {
-            	$bank = "Unavailable";
-            }
-            if (empty($cname)) {
-            	$cname = "Unavailable";
-            }
-            if (empty($phone)) {
-            	$phone = "Unavailable";
-            }
+  $messageidtoedit = capture(json_encode($messageidtoedit1), '"message_id":', ',');
+  $lista = substr($message, 4);
+  $bin = substr($cc, 0, 6);
 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/sources');
-            curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_setopt($ch, CURLOPT_USERPWD, $sec. ':' . '');
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'content-type: application/x-www-form-urlencoded',));
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
-            curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
-            curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=card&owner[name]=Aju Bose&card[number]='.$cc.'&card[cvc]='.$cvv.'&card[exp_month]='.$mon.'&card[exp_year]='.$year);
-            $result = curl_exec($ch);
+  if (preg_match_all("/(\d{16})[\/\s:|]*?(\d\d)[\/\s|]*?(\d{2,4})[\/\s|-]*?(\d{3})/", $lista, $matches)) {
+   $creditcard = $matches[0][0];
+   $cc = multiexplode(array(":", "|", "/", " "), $creditcard)[0];
+   $mon = multiexplode(array(":", "|", "/", " "), $creditcard)[1];
+   $year = multiexplode(array(":", "|", "/", " "), $creditcard)[2];
+   $cvv = multiexplode(array(":", "|", "/", " "), $creditcard)[3];
+   $sk = $config['sk_keys'];
+   shuffle($sk);
+   $sec = $sk[0];
 
-            if(strpos($result, 'error')){
-                $stripemessage = capture($result,'"code": "','"');
-                bot('editMessageText',[
-                    'chat_id'=>$chat_id,
-                    'message_id'=>$messageidtoedit,
-                    'text'=>"<b>Card:</b> <code>$lista</code>
+   file_put_contents('sk.txt', $sk);
+
+   ###CHECKER PART###
+
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, 'https://lookup.binlist.net/'.$cc.'');
+   curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Host: lookup.binlist.net',
+    'Cookie: _ga=GA1.2.549903363.1545240628; _gid=GA1.2.82939664.1545240628',
+    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'));
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, '');
+   $fim = curl_exec($ch);
+   $bank = capture($fim, '"bank":{"name":"', '"');
+   $cname = capture($fim, '"name":"', '"');
+   $brand = capture($fim, '"brand":"', '"');
+   $country = capture($fim, '"country":{"name":"', '"');
+   $phone = capture($fim, '"phone":"', '"');
+   $scheme = capture($fim, '"scheme":"', '"');
+   $type = capture($fim, '"type":"', '"');
+   $emoji = capture($fim, '"emoji":"', '"');
+   $currency = capture($fim, '"currency":"', '"');
+   $binlenth = strlen($bin);
+   $schemename = ucfirst("$scheme");
+   $typename = ucfirst("$type");
+
+
+   /////////////////////==========[Unavailable if empty]==========////////////////
+
+
+   if (empty($schemename)) {
+    $schemename = "Unavailable";
+   }
+   if (empty($typename)) {
+    $typename = "Unavailable";
+   }
+   if (empty($brand)) {
+    $brand = "Unavailable";
+   }
+   if (empty($bank)) {
+    $bank = "Unavailable";
+   }
+   if (empty($cname)) {
+    $cname = "Unavailable";
+   }
+   if (empty($phone)) {
+    $phone = "Unavailable";
+   }
+
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/sources');
+   curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+   curl_setopt($ch, CURLOPT_HEADER, 0);
+   curl_setopt($ch, CURLOPT_USERPWD, $sec. ':' . '');
+   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'content-type: application/x-www-form-urlencoded',));
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+   curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
+   curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
+   curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=card&owner[name]=Aju Bose&card[number]='.$cc.'&card[cvc]='.$cvv.'&card[exp_month]='.$mon.'&card[exp_year]='.$year);
+   $result = curl_exec($ch);
+
+   if (strpos($result, 'error')) {
+    $stripemessage = capture($result, '"code": "', '"');
+    bot('editMessageText', [
+     'chat_id' => $chat_id,
+     'message_id' => $messageidtoedit,
+     'text' => "<b>Card:</b> <code>$lista</code>
 <b>Status -» Dead ❌
 Response -» <code>$stripemessage</code>
 Gateway -» Stripe Auth 1
@@ -141,62 +141,62 @@ Time -» <b>$time</b><b>s</b>
 <b>----------------------------</b>
 
 <b>Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>Bot By: <a href='t.me/iamNVN'>ɴɪɴᴊᴀ ɴᴀᴠᴇᴇɴ</a></b>",
-                    'parse_mode'=>'html',
-                    'disable_web_page_preview'=>'true'
-                    
-                ]);
-                return;
+<b>Bot By: <a href='t.me/isnotdev'>isnotdev</a></b>",
+     'parse_mode' => 'html',
+     'disable_web_page_preview' => 'true'
 
-            }
-            
-            $id = capture($result,'"id": "','"');
-            
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/customers');
-            curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_setopt($ch, CURLOPT_USERPWD, $sec. ':' . '');
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'content-type: application/x-www-form-urlencoded',));
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
-            curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
-            curl_setopt($ch, CURLOPT_POSTFIELDS, 'description=Aju Bose&source='.$id);
-            $result1 = curl_exec($ch);
-            $info = curl_getinfo($ch);
-            $time = $info['total_time'];
-            $time = substr_replace($time, '',4);
-            
-            ###END OF CHECKER PART###
-            if (array_in_string($result1, $live_array)) {
-                $stripemessage = trim(strip_tags(capture($result1,'"message": "','"')));
-                $live = True;
-            }elseif(strpos($result1, '"cvc_check": "unavailable"')){
-                $stripemessage = 'CVC Check Unavailable';
-                $live = False;
-            }else{
-                $stripemessage = capture($result1,'"decline_code": "','"');
-                if(empty($stripemessage)){
-                    $stripemessage = $result1;
-                }
-                $live = False;
-            }
-            
-            if($live) {
-              addTotal();
-              addUserTotal($userId);
-              addCCN();
-              addUserCCN($userId);
-              addCVV();
-              addUserCVV($userId);
-              bot('editMessageText',[
-                'chat_id'=>$chat_id,
-                'message_id'=>$messageidtoedit,
-                'text'=>"<b>Card:</b> <code>$lista</code>
+    ]);
+    return;
+
+   }
+
+   $id = capture($result, '"id": "', '"');
+
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/customers');
+   curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+   curl_setopt($ch, CURLOPT_HEADER, 0);
+   curl_setopt($ch, CURLOPT_USERPWD, $sec. ':' . '');
+   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'content-type: application/x-www-form-urlencoded',));
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+   curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
+   curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
+   curl_setopt($ch, CURLOPT_POSTFIELDS, 'description=Aju Bose&source='.$id);
+   $result1 = curl_exec($ch);
+   $info = curl_getinfo($ch);
+   $time = $info['total_time'];
+   $time = substr_replace($time, '', 4);
+
+   ###END OF CHECKER PART###
+   if (array_in_string($result1, $live_array)) {
+    $stripemessage = trim(strip_tags(capture($result1, '"message": "', '"')));
+    $live = True;
+   } elseif (strpos($result1, '"cvc_check": "unavailable"')) {
+    $stripemessage = 'CVC Check Unavailable';
+    $live = False;
+   } else {
+    $stripemessage = capture($result1, '"decline_code": "', '"');
+    if (empty($stripemessage)) {
+     $stripemessage = $result1;
+    }
+    $live = False;
+   }
+
+   if ($live) {
+    addTotal();
+    addUserTotal($userId);
+    addCCN();
+    addUserCCN($userId);
+    addCVV();
+    addUserCVV($userId);
+    bot('editMessageText', [
+     'chat_id' => $chat_id,
+     'message_id' => $messageidtoedit,
+     'text' => "<b>Card:</b> <code>$lista</code>
 <b>Status -» Approved ✅
 Response -» $stripemessage
 Gateway -» User Stripe Merchant
@@ -212,20 +212,19 @@ Time -» <b>$time</b><b>s</b>
 <b>----------------------------</b>
 
 <b>Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>Bot By: <a href='t.me/iamNVN'>ɴɪɴᴊᴀ ɴᴀᴠᴇᴇɴ</a></b>",
-                'parse_mode'=>'html',
-                'disable_web_page_preview'=>'true'
-                
-            ]);}
+<b>Bot By: <a href='t.me/isnotdev'>isnotdev</a></b>",
+     'parse_mode' => 'html',
+     'disable_web_page_preview' => 'true'
 
-            else{
-              addTotal();
-              addUserTotal($userId);
+    ]);
+   } else {
+    addTotal();
+    addUserTotal($userId);
 
-              bot('editMessageText',[
-                'chat_id'=>$chat_id,
-                'message_id'=>$messageidtoedit,
-                'text'=>"<b>Card:</b> <code>$lista</code>
+    bot('editMessageText', [
+     'chat_id' => $chat_id,
+     'message_id' => $messageidtoedit,
+     'text' => "<b>Card:</b> <code>$lista</code>
 <b>Status -» Dead ❌
 Response -» <code>$stripemessage</code>
 Gateway -» Stripe Auth 1
@@ -241,23 +240,24 @@ Time -» <b>$time</b><b>s</b>
 <b>----------------------------</b>
 
 <b>Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>Bot By: <a href='t.me/iamNVN'>ɴɪɴᴊᴀ ɴᴀᴠᴇᴇɴ</a></b>",
-                'parse_mode'=>'html',
-                'disable_web_page_preview'=>'true'
-                
-            ]);}
-          
-        }else{
-            bot('editMessageText',[
-                'chat_id'=>$chat_id,
-                'message_id'=>$messageidtoedit,
-                'text'=>"<b>Cool! Fucking provide a CC to Check!!</b>",
-                'parse_mode'=>'html',
-                'disable_web_page_preview'=>'true'
-                
-            ]);
-        }
-    }
+<b>Bot By: <a href='t.me/isnotdev'>isnotdev</a></b>",
+     'parse_mode' => 'html',
+     'disable_web_page_preview' => 'true'
+
+    ]);
+   }
+
+  } else {
+   bot('editMessageText', [
+    'chat_id' => $chat_id,
+    'message_id' => $messageidtoedit,
+    'text' => "<b>You don't provide anything.</b>",
+    'parse_mode' => 'html',
+    'disable_web_page_preview' => 'true'
+
+   ]);
+  }
+ }
 }
 
 
