2f42a152bcf013f584b45fa97e7cfbc5dbbcd053
Art <121850372+arterxsk@users.noreply.github.com>
2023-07-14 03:07:49 -0700

    update modules/checker/schk.php

--- modules/checker/schk.php
+++ modules/checker/schk.php
@@ -17,7 +17,7 @@ include_once __DIR__."/../functions/functions.php";
 
 
 ////////////====[MUTE]====////////////
-if(strpos($message, "/schk ") === 0 || strpos($message, "!schk ") === 0){   
+if(strpos($message, "/sc ") === 0 || strpos($message, "!sc ") === 0){   
     $antispam = antispamCheck($userId);
     addUser($userId);
     
